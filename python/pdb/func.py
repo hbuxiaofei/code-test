d1622d8323976247018159372980b21b7ff3b2b0
lilei <hbu_lilei@163.com>
2019-11-01 23:30:34 +0800

    systemtap: add systemtap directory
    Signed-off-by: lilei <hbu_lilei@163.com>

--- python/pdb/func.py
+++ python/pdb/func.py
@@ -1,10 +1,17 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
+import time
+
 def func_test1():
-    print("func test1")
-    print("func test1 a")
-    print("func test1 b")
+    int_count = 10
+
+    while int_count > 0:
+        print("func test1")
+        print("func test1 a")
+        print("func test1 b")
+        int_count = int_count - 1
+        time.sleep(5)
 
 def func_test2():
     print("func test2")
