23bfd2c94858d84c9aec89dffa00b5c8bf3071e1
lilei <lilei@htzg.com>
2019-12-09 02:08:21 -0500

    gtk: add xorg example
    Signed-off-by: lilei <lilei@htzg.com>

--- go/gtk/test1/start_xorg.sh
+++ go/gtk/test1/start_xorg.sh
@@ -2,6 +2,11 @@
 
 # https://github.com/rhinstaller/anaconda/pyanaconda/display.py
 # DISPLAY=:1 ./app
+#
+# Other example:
+#   /usr/bin/Xorg :1 vt1 -keeptty
+#   systemctl restart console-gettty
+#
 X_DISPLAY_NUMBER=1
 /usr/bin/Xorg -br -logfile /tmp/X.log :$X_DISPLAY_NUMBER vt1 -s 1440 \
     -ac -nolisten tcp -dpi 96 -noreset
