0224e10db6b9f704bf0264461d4b47aca10052e2
lilei <hbu_lilei@163.com>
2020-02-06 22:58:28 +0800

    python: add move step to chess_top of chapter4-1
    Signed-off-by: lilei <hbu_lilei@163.com>

---
+++ python/chess_top/chapter3-6/4-1.py
@@ -0,0 +1,490 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+# ---------------变量声明-----------------------------------------------
+side = 0                   # 轮到哪方走，0表示红方，1表示黑方
+board = [0]*256            # 棋盘数组
+FenString = ""             # 局面的FEN串格式
+
+class move(object):
+    def __init__(self):
+        self.from_pos = 0
+        self.to_pos = 0
+
+MoveArray = []      # 走法数组
+for i in range(128):
+    MoveArray.append(move())
+MoveNum = 1;        # 走法总数，从1计数
+
+
+# ----------------走法生成相关辅助数组---------------------------------------------
+#各种棋子走法数组
+KingDir = [-0x10, -0x01, +0x01, +0x10, 0, 0, 0, 0]     #将
+AdvisorDir = [-0x11, -0x0f, +0x0f, +0x11, 0, 0, 0, 0]  #士
+BishopDir =[-0x22, -0x1e, +0x1e, +0x22,	0, 0, 0, 0]    #象
+KnightDir =[-0x21, -0x1f, -0x12, -0x0e,	+0x0e, +0x12, +0x1f, +0x21] #马
+RookDir =[-0x01, +0x01, -0x10, +0x10, 0, 0, 0, 0]                   #车
+CannonDir =[-0x01, +0x01, -0x10, +0x10,	0, 0, 0, 0]#炮
+PawnDir = [[-0x01, +0x01, -0x10, 0, 0, 0, 0, 0],
+           [-0x01, +0x01, +0x10, 0, 0, 0, 0, 0]]   #兵
+
+KnightCheck = [-0x10,-0x10,-0x01,+0x01,-0x01,+0x01,+0x10,+0x10]#马腿位置
+BishopCheck = [-0x11,-0x0f,+0x0f,+0x11,0,0,0,0] #象眼位置
+kingpalace = [54,55,56,70,71,72,86,87,88]       #黑方九宫位置
+
+
+
+#各种棋子合理位置数组
+KingPosition = [
+        0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+        0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+        0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+        0, 0, 0, 0, 0, 0, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0,
+        0, 0, 0, 0, 0, 0, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0,
+        0, 0, 0, 0, 0, 0, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0,
+        0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+        0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+        0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+        0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+        0, 0, 0, 0, 0, 0, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0,
+        0, 0, 0, 0, 0, 0, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0,
+        0, 0, 0, 0, 0, 0, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0,
+        0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+        0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+        0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0
+        ]
+
+AdvisorPisition = [
+        0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+        0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+        0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+        0, 0, 0, 0, 0, 0, 1, 0, 1, 0, 0, 0, 0, 0, 0, 0,
+        0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0,
+        0, 0, 0, 0, 0, 0, 1, 0, 1, 0, 0, 0, 0, 0, 0, 0,
+        0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+        0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+        0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+        0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+        0, 0, 0, 0, 0, 0, 1, 0, 1, 0, 0, 0, 0, 0, 0, 0,
+        0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0,
+        0, 0, 0, 0, 0, 0, 1, 0, 1, 0, 0, 0, 0, 0, 0, 0,
+        0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+        0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+        0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0
+        ]
+
+BishopPosition = [
+        0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+        0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+        0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+        0, 0, 0, 0, 0, 1, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0,
+        0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+        0, 0, 0, 1, 0, 0, 0, 1, 0, 0, 0, 1, 0, 0, 0, 0,
+        0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+        0, 0, 0, 0, 0, 1, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0,
+        0, 0, 0, 0, 0, 1, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0,
+        0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+        0, 0, 0, 1, 0, 0, 0, 1, 0, 0, 0, 1, 0, 0, 0, 0,
+        0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+        0, 0, 0, 0, 0, 1, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0,
+        0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+        0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+        0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0
+        ]
+
+KnightPosition = [
+        0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+        0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+        0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+        0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0,
+        0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0,
+        0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0,
+        0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0,
+        0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0,
+        0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0,
+        0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0,
+        0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0,
+        0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0,
+        0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0,
+        0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+        0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+        0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0
+        ]
+
+PawnPosition = [
+        [	# 红兵
+            0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+            0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+            0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+            0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0,
+            0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0,
+            0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0,
+            0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0,
+            0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0,
+            0, 0, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 0, 0, 0,
+            0, 0, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 0, 0, 0,
+            0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+            0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+            0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+            0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+            0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+            0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0
+            ],
+        [	# 黑兵
+            0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+            0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+            0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+            0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+            0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+            0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+            0, 0, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 0, 0, 0,
+            0, 0, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 0, 0, 0,
+            0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0,
+            0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0,
+            0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0,
+            0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0,
+            0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0,
+            0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+            0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+            0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0
+            ]
+        ]
+
+
+# --------------------函数实现----------------------------------
+
+# 棋子整数值转换成字符表示
+def IntToChar(a):
+    """  棋子整数值转换成字符表示 """
+    if (a < 32):
+        if a == 16:
+            return 'K'
+        elif a == 17 or a == 18:
+            return 'A'
+        elif a == 19 or a == 20:
+            return 'B'
+        elif a == 21 or a == 22:
+            return 'N'
+        elif a == 23 or a == 24:
+            return 'R'
+        elif a == 25 or a == 26:
+            return 'C'
+        elif a == 27 or a == 28 or a == 29 or a == 30 or a == 31:
+            return 'P'
+        else:
+            return 0
+    else:
+        a = a - 16
+        if a == 16:
+            return 'k'
+        elif a == 17 or a == 18:
+            return 'a'
+        elif a == 19 or a == 20:
+            return 'b'
+        elif a == 21 or a == 22:
+            return 'n'
+        elif a == 23 or a == 24:
+            return 'r'
+        elif a == 25 or a == 26:
+            return 'c'
+        elif a == 27 or a == 28 or a == 29 or a == 30 or a == 31:
+            return 'p'
+        else:
+            return 0
+
+
+def ClearBoard():
+    """ 清空棋盘数组board """
+    for i in range(len(board)):
+        board[i] = 0
+
+
+def CharToSubscript(ch):
+    """
+    FEN串中棋子对应的数组下标
+    下标0，1，2，3，4，5，6分别对应表示将，仕，象，马，车，炮，兵
+    """
+    if ch == 'k' or ch == 'K':
+        return 0
+    elif ch == 'a' or ch == 'A':
+        return 1
+    elif ch == 'b' or ch == 'B':
+        return 2
+    elif ch == 'n' or ch == 'N':
+        return 3
+    elif ch == 'r' or ch == 'R':
+        return 4
+    elif ch == 'c' or ch == 'C':
+        return 5
+    elif ch == 'p' or ch == 'P':
+        return 6
+    else:
+        return 7
+
+
+def StringToArray(FenStr):
+    """
+    将FEN串表示的局面转换成一维数组
+    """
+    pcWhite = [16, 17, 19, 21, 23, 25, 27]
+    pcBlack = [32, 33, 35, 37, 39, 41, 43]
+
+    if len(FenStr) == 0:
+        return
+
+    ClearBoard()
+
+    fen_list = list(FenStr)
+    i = 3
+    j = 3
+    for index in range(len(fen_list)):
+        if fen_list[index] == ' ':
+            break
+
+        if fen_list[index] == '/':
+            j = 3
+            i = i + 1
+            if i > 12:
+                break
+        elif fen_list[index] >= '1' and fen_list[index] <= '9':
+            for k in range(ord(fen_list[index]) - ord('0')):
+                if (j >= 11):
+                    break;
+                j = j + 1
+        elif fen_list[index] >= 'A' and fen_list[index] <= 'Z':
+            if (j <= 11):
+                k = CharToSubscript(fen_list[index])
+                if (k < 7):
+                    if (pcWhite[k] < 32):
+                        board[(i<<4) + j] = pcWhite[k]
+                        pcWhite[k] = pcWhite[k] + 1
+                j = j + 1
+        elif fen_list[index] >= 'a' and fen_list[index] <= 'z':
+            if (j <= 11):
+                k = CharToSubscript(fen_list[index])
+                if (k < 7):
+                    if (pcBlack[k] < 48):
+                        board[(i<<4) + j] = pcBlack[k]
+                        pcBlack[k] = pcBlack[k] + 1
+                j = j + 1
+
+    index = index + 1
+    if (fen_list[index] == 'b'):
+        side = 1
+    else:
+        side = 0
+
+
+def ArrayToString(bd):
+    """
+    将一维数组表示的局面转换成FEN串
+    """
+
+    fen_list = [' ']*256
+    index = 0
+    for i in range(3, 13):
+        k = 0
+        for j in range(3, 12):
+            pc = bd[(i << 4) + j]
+            if (pc != 0):
+                if (k > 0):
+                    fen_list[index] = chr(k + ord('0'))
+                    index = index + 1
+                    k = 0
+                fen_list[index] = IntToChar(pc)
+                index = index + 1
+            else:
+                k = k + 1
+        if (k > 0):
+            fen_list[index] = chr(k + ord('0'))
+            index = index + 1
+        fen_list[index] = '/'
+        index = index + 1
+    index = index - 1
+    fen_list[index] = ' '
+    index = index + 1
+    if side == 0:
+        fen_list[index] = 'w'
+    else:
+        fen_list[index] = 'b'
+
+    return ("".join(fen_list))
+
+
+def OutputBoard():
+    """ 输出棋盘数组 """
+    for i in range(1, len(board)+1):
+        print("%2d" % board[i-1], end=' ')
+        if (i%16 == 0):
+            print("")
+
+
+def InitGen():
+    global MoveNum
+    MoveNum = 0
+
+
+def SaveMove(from_pos, to_pos):
+    global MoveNum
+    MoveArray[MoveNum].from_pos = from_pos
+    MoveArray[MoveNum].to_pos = to_pos
+    MoveNum = MoveNum + 1
+
+
+def KingMove(p):
+    """
+    将走棋
+    """
+    SideTag = 16 + side * 16 # 走棋方，红方16，黑方32
+    for k in range(4):       # 4个方向
+        n = p + KingDir[k]   #n为新的可能走到的位置
+        if KingPosition[n]:
+            if (not (board[n] & SideTag)): #目标位置上没有本方棋子
+                SaveMove(p, n)
+
+
+def AdvisorMove(p):
+    """
+    仕走棋
+    """
+    SideTag = 16 + side * 16  # 走棋方，红方16，黑方32
+    for k in range(4):        # 4个方向
+        n = p + AdvisorDir[k] # n为新的可能走到的位置
+        if AdvisorPisition[n]:
+            if (not (board[n] & SideTag)): # 目标位置上没有本方棋子
+                SaveMove(p, n)
+
+
+def BishopMove(p):
+    """
+    相走棋
+    """
+    SideTag = 16 + side * 16#走棋方，红方16，黑方32
+    for k in range(4):        # 4个方向
+        n = p + BishopDir[k]#n为新的可能走到的位置
+        if (BishopPosition[n]):
+            m = p + BishopCheck[k]
+            if not board[m]: #象眼位置无棋子占据
+                if (not (board[n] & SideTag)):#目标位置上没有本方棋子
+                    SaveMove(p, n);
+
+
+def KnightMove(p):
+    """
+    马走棋
+    """
+    SideTag = 16 + side * 16#走棋方，红方16，黑方32
+    for k in range(8):        # 8个方向
+        n = p + KnightDir[k]  #n为新的可能走到的位置
+        if KnightPosition[n]:
+            m = p + KnightCheck[k] # 马腿位置
+            if (not board[m]):     #马腿位置无棋子占据
+                if (not (board[n] & SideTag)):#目标位置上没有本方棋子
+                    SaveMove(p, n)
+
+
+def RookMove(p):
+    """
+    车走棋
+    """
+    SideTag = 16 + side * 16    #走棋方，红方16，黑方32
+    for k in range(4):          # 4个方向
+        for j in range(1, 10):  # 横的最多有8个可能走的位置，纵向最多有9个位置
+            n = p + j * RookDir[k]
+            if (not KnightPosition[n]): #不合理的位置
+                break
+            if (not board[n]): # 目标位置上无子
+                SaveMove(p, n)
+            elif (board[n] & SideTag): # 目标位置上有本方棋子
+                break
+            else: # 目标位置上有对方棋子
+                SaveMove(p, n)
+                break
+
+
+def CannonMove(p):
+    """
+    炮走棋
+    """
+    SideTag = 16 + side * 16    # 走棋方，红方16，黑方32
+    for k in range(4):          # 4个方向
+        OverFlag = 0
+        for j in range(1, 10):  # 横的最多有8个可能走的位置，纵向最多有9个位置
+            n = p + j * CannonDir[k]
+            if (not KnightPosition[n]):  # 不合理的位置
+                break
+            if (not board[n]):     # 目标位置上无子
+                if (not OverFlag): # 未翻山
+                    SaveMove(p, n)
+                # 已翻山则不作处理，自动考察向下一个位置
+            else: # 目标位置上有子
+                if (not OverFlag): # 未翻山则置翻山标志
+                    OverFlag = 1
+                else: # 已翻山
+                    if (not (board[n] & SideTag)): # 对方棋子
+                        SaveMove(p, n)
+                    break  # 不论吃不吃子，都退出此方向搜索
+
+
+def PawnMove(p):
+    """
+    卒走棋
+    """
+    SideTag = 16 + side * 16 # 走棋方，红方16，黑方32
+    for k in range(3): #3个方向
+        n = p + PawnDir[side][k] # n为新的可能走到的位置
+        if (PawnPosition[side][n]):
+            if (not (board[n] & SideTag)): # 目标位置上没有本方棋子
+                SaveMove(p, n)
+
+
+def GenAllMove():
+    SideTag = 16 + side * 16   # 走棋方，红方16，黑方32
+    for i in range(3, 13):     # 10 行
+        for j in range(3, 12): # 9列
+            p = (i<<4) + j     # 棋子位置
+            if (not (board[p] & SideTag)):  # 对方的棋子
+                continue
+
+            kind = board[p] - SideTag
+
+            if kind == 0:   # 将
+                KingMove(p)
+            elif kind == 1 or kind == 2:  # 仕
+                AdvisorMove(p)
+            elif kind == 3 or kind == 4:  # 相
+                BishopMove(p)
+            elif kind == 5 or kind == 6:  # 马
+                KnightMove(p)
+            elif kind == 7 or kind == 8:  # 车
+                RookMove(p)
+            elif kind == 9 or kind == 10: # 炮
+                CannonMove(p)
+            elif kind == 11 or kind == 12 or kind == 13 or kind == 14 or kind == 15: # 兵
+                PawnMove(p)
+
+
+def OutputMove():
+    global MoveNum
+    for i in range(MoveNum):
+        print("from %d to %d" % (MoveArray[i].from_pos, MoveArray[i].to_pos))
+    print("total move number:%d" % MoveNum)
+
+
+def test_main():
+    print("****************************************************************")
+    print(" 示例程序4-1             走 法 生 成")
+    print("")
+    print("****************************************************************")
+    print("初始局面:")
+    StringToArray("rnbakabnr/9/1c5c1/p1p1p1p1p/9/9/P1P1P1P1P/1C5C1/9/RNBAKABNR w - - 0 1")
+    OutputBoard()
+    print("生成的走法:")
+    InitGen()
+    GenAllMove()
+    OutputMove()
+
+
+
+if __name__ == "__main__":
+    test_main()
