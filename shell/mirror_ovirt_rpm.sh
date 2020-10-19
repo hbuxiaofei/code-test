0f2831e49d2bcfd3610ba457852dc8a1de19c52e
raylee <rayylee@foxmail.com>
2020-10-19 06:22:53 -0400

    shell: modify mirror ovirt rpm script, do not download same rpm package

--- shell/mirror_ovirt_rpm.sh
+++ shell/mirror_ovirt_rpm.sh
@@ -27,10 +27,12 @@ while read _line; do
     _download=$(cat noarch.rpm.list | grep "${_path}/${_name}-" 2>/dev/null | tail -n1)
     echo ">>> $_download"
 
-    mwget -n 20 -d noarch ${_download}
+    if [ ! -e noarch/${_package} ]; then
+        mwget -n 20 -d noarch ${_download}
+    else
+        echo "File already exist: noarch/${_package}"
+    fi
 done < noarch.rpm.list
-
-exit 0
 
 cat mirror.isoc.org.il/pub/ovirt/ovirt-4.4/rpm/el8/x86_64 | grep -oP "http://.*?\.rpm" > x86_64.rpm.list
 while read _line; do
@@ -42,5 +44,9 @@ while read _line; do
     _download=$(cat x86_64.rpm.list | grep "${_path}/${_name}-" 2>/dev/null | tail -n1)
     echo ">>> $_download"
 
-    mwget -n 20 -d x86_64 ${_download}
+    if [ ! -e x86_64/${_package} ]; then
+        mwget -n 20 -d x86_64 ${_download}
+    else
+        echo "File already exist: x86_64/${_package}"
+    fi
 done < x86_64.rpm.list
