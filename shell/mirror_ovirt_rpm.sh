d144e2e6251f72e7a270211433e0cfc17ef8f826
raylee <rayylee@foxmail.com>
2020-10-19 21:58:19 -0400

    shell: modify mirror ovirt rpm script, more fast download

--- shell/mirror_ovirt_rpm.sh
+++ shell/mirror_ovirt_rpm.sh
@@ -19,34 +19,34 @@ wget -c -r -np -k -L -p \
 
 cat mirror.isoc.org.il/pub/ovirt/ovirt-4.4/rpm/el8/noarch | grep -oP "http://.*?\.rpm" > noarch.rpm.list
 while read _line; do
-    echo ">>> download:"
+    echo ">>> download :"
 
     _path="${_line%/*}"
     _package="${_line##*/}"
     _name=$(echo "${_package##*/}" | sed -e s/-[[:digit:]]./@/ | awk -F '@' '{print $1}')
-    _download=$(cat noarch.rpm.list | grep "${_path}/${_name}-" 2>/dev/null | tail -n1)
+    _download=$(grep "${_path}/${_name}-[0-9]" noarch.rpm.list 2>/dev/null | tail -n1)
     echo ">>> $_download"
 
-    if [ ! -e noarch/${_package} ]; then
+    if [ ! -e "noarch/${_download##*/}" ]; then
         mwget -n 20 -d noarch ${_download}
     else
-        echo "File already exist: noarch/${_package}"
+        echo "File exist: noarch/${_download##*/}"
     fi
 done < noarch.rpm.list
 
 cat mirror.isoc.org.il/pub/ovirt/ovirt-4.4/rpm/el8/x86_64 | grep -oP "http://.*?\.rpm" > x86_64.rpm.list
 while read _line; do
-    echo ">>> download:"
+    echo ">>> download :"
 
     _path="${_line%/*}"
     _package="${_line##*/}"
     _name=$(echo "${_package##*/}" | sed -e s/-[[:digit:]]./@/ | awk -F '@' '{print $1}')
-    _download=$(cat x86_64.rpm.list | grep "${_path}/${_name}-" 2>/dev/null | tail -n1)
+    _download=$(grep "${_path}/${_name}-[0-9]" x86_64.rpm.list 2>/dev/null | tail -n1)
     echo ">>> $_download"
 
-    if [ ! -e x86_64/${_package} ]; then
+    if [ ! -e "x86_64/${_download##*/}" ]; then
         mwget -n 20 -d x86_64 ${_download}
     else
-        echo "File already exist: x86_64/${_package}"
+        echo "File exist: x86_64/${_download##*/}"
     fi
 done < x86_64.rpm.list
