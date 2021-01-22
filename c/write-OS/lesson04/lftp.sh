3aa1c25271438cee5d0196d4e9eca8470360a707
rayylee <rayylee@foxmail.com>
2021-01-21 21:47:33 -0500

    c: Add lesson04 for write-os, (No assignment) Set up 8259A and use 0xB8000 Mem

---
+++ c/write-OS/lesson04/lftp.sh
@@ -0,0 +1,40 @@
+#!/usr/bin/env bash
+
+#!/usr/bin/env bash
+
+# lftp user@host
+#      >mirror remotedir localdir          ：本地下载远端文件夹
+#
+#      >mirror -R localdir remotedir       :-R 本地上传文件夹到远端
+
+
+[ -d  bootimage ] && rm -rf bootimage
+mkdir bootimage
+
+cp -f bootsect.s bootimage/
+cp -f binary.s bootimage/
+cp -f setup.s bootimage/
+cp -f Makefile bootimage/
+
+lftp <<EOF
+open ftp://wwftp:wwftp@192.168.1.252
+mv /lilei/rinux/binary.s /lilei/rinux/1
+mv /lilei/rinux/setup.s /lilei/rinux/1
+mv /lilei/rinux/bootsect.s /lilei/rinux/1
+mv /lilei/rinux/Makefile /lilei/rinux/1
+close
+bye
+EOF
+
+lftp <<EOF
+open ftp://wwftp:wwftp@192.168.1.252
+mirror -R bootimage /lilei/rinux
+close
+bye
+EOF
+
+[ -d  bootimage ] && rm -rf bootimage
+make clean
+
+exit 0
+
