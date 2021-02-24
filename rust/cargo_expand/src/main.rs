0dac8597d6c07e27e898954ca318ed37a0f4c678
rayylee <rayylee@foxmail.com>
2021-02-23 22:59:45 -0500

    rust: modify macro example

--- rust/cargo_expand/src/main.rs
+++ rust/cargo_expand/src/main.rs
@@ -1,3 +1,7 @@
+// stringify! 作用是将其参数直接转换为字符串描述，类型为&'static str
+// let one_plus_one = stringify!(1 + 1);
+// assert_eq!(one_plus_one, "1 + 1");
+
 macro_rules! m {
     ($($s:stmt)*) => {
         $(
--- rust/macro/select_one.rs
+++ rust/macro/select_one.rs
@@ -1,3 +1,8 @@
+//Ref: http://blog.hubwiz.com/2020/01/30/rust-macro
+//     https://medium.com/@phoomparin/a-beginners-guide-to-rust-macros-5c75594498f1
+
+use std::collections::HashMap;
+
 // 下面是一些常见的 Rust宏选择器：
 //
 // item  ：条目，例如函数、结构、模块等
@@ -36,9 +41,29 @@ fn say_yo_example() {
     say_yo!("Carl");
 }
 
+macro_rules! macro_map {
+    ($( $key:expr => $value:expr ), *) => { {
+        let mut hm = HashMap::new();
+        $( hm.insert($key, $value); )*
+            hm
+    }};
+}
+
+fn macro_map_example() {
+    let user = macro_map!( "name" => "Carl", "gender" => "Boy");
+    println!(
+        "User:{:?}, name:{} , gender:{}",
+        user,
+        user.get("name").unwrap(),
+        user.get("gender").unwrap()
+    );
+}
+
 fn main() {
     println!(">>> select_one example:");
     select_one_example();
     println!(">>> say_yo example:");
     say_yo_example();
+    println!(">>> macro_map example:");
+    macro_map_example();
 }
